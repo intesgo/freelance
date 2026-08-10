45d02135301fd158280a0463d28050c979e390c4
intesgo <intesgo@gmail.com>
2026-08-10 17:23:51 -0500

    Add files via upload

--- pruebas/test_cambios_419.js
+++ pruebas/test_cambios_419.js
@@ -11,7 +11,7 @@ const probar = (ok, mensaje) => {
 };
 
 probar(/const VERSION = \{ n:"425"/.test(app), "la aplicación debe anunciar v425");
-probar(/const CACHE = "freelance-v215"/.test(sw), "el service worker debe renovar la caché a v215");
+probar(/const CACHE = "freelance-v216"/.test(sw), "el service worker debe renovar la caché a v216");
 
 probar(/className="busc-grande proveedor-destacado"/.test(app), "el buscador de proveedor debe estar resaltado");
 probar(/items=\{PROVS_PED\} sinLabel value=\{prov\} multiPalabra/.test(app), "el proveedor debe buscar por palabras sueltas");
